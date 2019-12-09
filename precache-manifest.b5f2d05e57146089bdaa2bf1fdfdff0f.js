f4900f5f31c282d51f9ac4044e1a36793ddcd719
balraaz15 <balraaz11@gmail.com>
2019-12-10 00:55:43 +0545

    Updates

---
+++ precache-manifest.b5f2d05e57146089bdaa2bf1fdfdff0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d3195dd80dcbae1af8ecebdf3cf096",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16001d5782569be11856",
+    "url": "/monsters-rolodex/static/css/main.46cc9ab6.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16001d5782569be11856",
+    "url": "/monsters-rolodex/static/js/main.58105b81.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
